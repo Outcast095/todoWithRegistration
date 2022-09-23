57ebda551c4f55ad3fe2ccfbaf4c4d1b26ef3941
Magomed <rostovmen95@gmail.com>
2022-09-23 19:21:34 +0300

    commit

--- src/components/userTodo/UserTodo.js
+++ src/components/userTodo/UserTodo.js
@@ -13,25 +13,33 @@ function UserTodo(props) {
 
     return (
         <div className={style.TodoStyle}>
-            <h2 className={style.UserNameTitle}>список ващих дел {props.userPosts.user}</h2>
-            <div className={style.InputArea}>
-                <form onSubmit={handleSubmit(onSubmitHandler)}>
-                    <input className={style.InputStyle}
-                           {...register("userTodo",
-                               {
-                                   required: "поле объязательно для заполнения",                           /// вместо труе мы можем записать текст,
-                               }
-                           )}
-                           placeholder={props.placeholder}
-                    />
-                    <input type="submit" className={style.ButtonStyle}/>
-                </form>
+            <div className={style.SydeBar}>
             </div>
-            <div>{props.userPosts.todos.map(post => {
-                        return <ul key={post.id}>
-                            <UserTodoItem post={post.textTodo} />
-                        </ul>})
-            }</div>
+            <div className={style.MainBlock}>
+                <div className={style.Burger} onClick={() => console.log("click")}>
+                    <img src="https://img.icons8.com/external-others-bomsymbols-/30/40C057/external-hamberger-flat-general-office-others-bomsymbols-.png"/>
+                </div>
+
+                <h2 className={style.UserNameTitle}>список ващих дел {props.userPosts.user}</h2>
+                <div className={style.InputArea}>
+                    <form onSubmit={handleSubmit(onSubmitHandler)}>
+                        <input className={style.InputStyle}
+                               {...register("userTodo",
+                                   {
+                                       required: "поле объязательно для заполнения",                           /// вместо труе мы можем записать текст,
+                                   }
+                               )}
+                               placeholder={props.placeholder}
+                        />
+                        <input type="submit" className={style.ButtonStyle}/>
+                    </form>
+                </div>
+                <div>{props.userPosts.todos.map(post => {
+                    return <ul key={post.id}>
+                        <UserTodoItem post={post.textTodo} />
+                    </ul>})
+                }</div>
+            </div>
         </div>
     );
 }

9254a20c16e45933d97b688f4315a17aae806450
Magomed95 <rostovmen95@gmail.com>
2022-10-07 20:27:24 +0300

    modalWindow

--- src/components/authorization/ModalWindow/ModalWindow.js
+++ src/components/authorization/ModalWindow/ModalWindow.js
@@ -1,9 +1,19 @@
 
 import styles from "./ModalWindow.module.css"
-import ButtonStyle from "../../button/Button.module.css"
+import ButtonStyle from "../../button/Button.module.css";
+
+import { useNavigate } from "react-router-dom";
+
 
 
 function ModalWindow() {
+
+    const navigate = useNavigate()
+
+    function returnToAuth () {
+        navigate(`/`)
+    }
+
     return (
         <div className={styles.ModalWrapperWindow}>
             <div className={styles.ModalBigWindow}></div>
@@ -11,7 +21,7 @@ function ModalWindow() {
                 <h2 className={styles.TextTitle}>поздравляю, вы успешно зарегистрировались !!!!</h2>
 
                 <div>
-                    <button className={ButtonStyle.ButtonStyle}>перейти на страницу авторизации</button>
+                    <button onClick={returnToAuth} className={ButtonStyle.ButtonStyle}>перейти на страницу авторизации</button>
                 </div>
             </div>
         </div>
